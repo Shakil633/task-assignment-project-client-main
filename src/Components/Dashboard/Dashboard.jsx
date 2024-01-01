79c5eb195f41e08ea06ff27af23a47bc30e9d847
unknown <saklislam633@gmail.com>
2024-01-01 13:15:59 +0600

    adds dashboard new button

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -24,10 +24,12 @@ const Dashboard = () => {
 
   return (
     <div>
+      {/* Header section */}
       <div className="flex lg:flex-row flex-col w-max mx-auto gap-7 items-center">
         <h1 className="text-2xl mb-[5vh] font-bold text-center mt-[5vh]">
           Task management dashboard
         </h1>
+        {/* Profile information */}
         <div
           id="profile-container"
           className="profile-picture text-black mr-[4%] cursor-pointer"
@@ -37,13 +39,18 @@ const Dashboard = () => {
         <p className="text-center">Name {user?.displayName}</p>
         <p className="text-center">Email {user?.email}</p>
       </div>
+
+      {/* Create Task button */}
       <Link
         to="/createTask"
         className="btn lg:top-[20vh] top-[37vh] flex justify-center items-center fixed right-5 bg-white 2xl:text-lg rounded-xl font-bold"
       >
         <p className="text-2xl 2xl:text-3xl">+</p> <p>Create Task</p>
       </Link>
+
+      {/* Task lists */}
       <div className="flex absolute flex-wrap">
+        {/* To-do list Tasks */}
         <div className="space-y-5 lg:w-[30vw] p-[2%]">
           <h3 className="text-xl font-bold text-center">To-do list Tasks</h3>
           <div>
@@ -64,6 +71,7 @@ const Dashboard = () => {
                   <p>{task.taskDescription}</p>
                   <button
                     className="btn btn-ghost btn-sm"
+                    onClick={() => handleDelete(task?._id)}
                     onTouchStart={(e) => {
                       e.preventDefault();
                       handleDelete(task?._id);
@@ -71,7 +79,8 @@ const Dashboard = () => {
                   >
                     Delete
                   </button>
-                  <button
+                  <Link
+                    to={`/update/${task?._id}`}
                     className="btn btn-ghost btn-sm"
                     onTouchStart={() => {
                       // Navigate to the edit page
@@ -79,15 +88,19 @@ const Dashboard = () => {
                     }}
                   >
                     Edit
-                  </button>
+                  </Link>
                 </div>
               </Draggable>
             ))}
           </div>
         </div>
+
+        {/* Ongoing Tasks */}
         <h3 className="text-xl absolute lg:top-0 top-[40vh] left-[10vw] lg:left-[40vw] w-[10vw] font-bold text-center">
           Ongoing Tasks
         </h3>
+
+        {/* Completed Tasks */}
         <h3 className="text-xl absolute top-[80vh] lg:pb-0 pb-[30vh] left-[10vw] lg:top-0 lg:left-[70vw] w-[10vw] font-bold text-center">
           Completed Tasks
         </h3>
